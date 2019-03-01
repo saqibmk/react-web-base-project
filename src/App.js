372de815f5ee51d29ace7e414a95357655234c9f
Saqib Khan <saqibmk@users.noreply.github.com>
2019-02-28 19:35:14 -0500

    Added Apollo Client to start using GraphQL

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home';
 import './App.css';
 
+// Configure your Apollo Client https://www.apollographql.com/docs/react/essentials/get-started.html#configuration
+const client = new ApolloClient({
+  uri: ''
+});
+
 const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-    </div>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  </ApolloProvider>
 );
 
 export default App;
